eb32dde29df47db0622a85bbbf2342bb551353be
Lucas raicovitch <lucasraicovitch@MacBook-Pro-de-Alice.local>
2019-10-24 15:36:22 +0200

    add final Shop

--- project/Components/ShopScreen.js
+++ project/Components/ShopScreen.js
@@ -6,38 +6,50 @@ class ShopScreen extends React.Component {
     constructor() {
         super();
         this.state = {
+          Mylife: 50,
           MyMoney: 100,  
-          potionPrice: 20,  
+          PotionPrice: 20, 
+          HealPower: 10, 
         }
       }
-    
-     // Store/Reducers/favoriteReducer.js
-    
-    
-    
+      
+
       PromptMyMoney = () => {
         var Money = this.state.MyMoney
         this.setState({
           MyMoney: Money
         })
       }
-    spendMoney = () => {
-        var MoneyNow = this.state.MyMoney - this.state.potionPrice
-        this.setState({
-            MyMoney: MoneyNow
-        })
-    }
+        BuyPotion = () => {
+           if (this.state.MyMoney >= this.state.PotionPrice){
+              var MoneyNow = this.state.MyMoney - this.state.PotionPrice
+                this.setState({
+                    MyMoney: MoneyNow
+                })
+                this.Heal();
+            }
+        }
+
+        Heal = () => {
+            var life = this.state.Mylife + this.state.HealPower
+            this.setState({
+                Mylife: life
+            })
+        }
+
+    
     
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <TouchableOpacity activeOpacity = { .5 } onPress={ this.spendMoney }>
+            <Text style={{ marginBottom: 10, fontSize: 20 }}>{this.state.Mylife} point de vie</Text>
+        <TouchableOpacity activeOpacity = { .5 } onPress={ this.BuyPotion }>
         <Image
           style={{width: 200, height: 200}}
           source={require('../img/potion.jpg')}
         />
         </TouchableOpacity>
-      <Text style={{ marginBottom: 10, fontSize: 20 }}>{this.state.MyMoney}</Text>
+      <Text style={{ marginBottom: 10, fontSize: 20 }}>{this.state.MyMoney} pièce d'or</Text>
       <Button
             title="Retour"
             onPress={() => this.props.navigation.goBack()}
