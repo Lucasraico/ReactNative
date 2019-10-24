19e838998bf3e5df481477908388ce1a2836e961
Unknown <quentin.valbeny@edu.itescia.fr>
2019-10-24 16:22:05 +0200

    Ajout un personnages

--- project/Hero/Hero.js
+++ project/Hero/Hero.js
@@ -5,14 +5,25 @@ import { View, Button } from 'react-native';
 export default class Hero extends Component {
     constructor() {
       super();
+<<<<<<< Updated upstream
   
+=======
+
+>>>>>>> Stashed changes
       this.state = {
         // This is our Default number value
         NumberHolder: 1,
         TextHolder: "",
         MyNumber: 1,
         Count: 0,
+<<<<<<< Updated upstream
   
       }
     }
 }
+=======
+
+      }
+    }
+}
+>>>>>>> Stashed changes
